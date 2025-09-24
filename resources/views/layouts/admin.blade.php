9e808f46b13d352dedc1ebd7558fb4679d99b793
lili <vecinocollililianamarisol@gmail.com>
2025-09-24 01:57:57 -0600

    feat(sidebar): add support for section headers in navigation links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,7 @@
 
             <!-- Scripts -->
             @vite(['resources/css/app.css', 'resources/js/app.js'])
+
             <script src="https://kit.fontawesome.com/5263ff3029.js" crossorigin="anonymous"></script>
 
             <!-- Styles -->
